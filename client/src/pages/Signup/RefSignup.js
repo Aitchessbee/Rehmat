4381be2d9b342ddd79b7974af1ce0fd6d99848e0
ARYAN DOGRA <95014165+aryandogra2003@users.noreply.github.com>
2023-02-26 04:24:50 +0530

    refugee signup commit
    change theme for refugee signup page

--- client/src/pages/Signup/RefSignup.js
+++ client/src/pages/Signup/RefSignup.js
@@ -4,18 +4,25 @@ import Button from "@mui/material/Button";
 // import IconButton from "@mui/material/IconButton";
 import FileUploadIcon from "@mui/icons-material/FileUpload";
 import Stack from "@mui/material/Stack";
+import wave from "./image/wave.png";
 
 function RefSignup() {
   return (
     <div className={Styles.container}>
-      <h1 style={{ fontSize: "3em" }}>UPLOAD YOUR REFUGEE CARD HERE </h1>
-      <Stack direction="row" alignItems="center" spacing={2}>
-        <Button className={Styles.btn1} variant="contained" component="label">
-          Upload
-          <input hidden accept="image/*" multiple type="file" />
-          <FileUploadIcon />
-        </Button>
-      </Stack>
+      <div style={{ zIndex: "0", position: "fixed", width: "100vw", top: "0" }}>
+        <img className={Styles.bgimg} src={wave} alt="wave png image"></img>
+      </div>
+      <div style={{ zIndex: "2" }}>
+        {" "}
+        <h1 style={{ fontSize: "3em" }}>UPLOAD YOUR REFUGEE CARD HERE </h1>
+        <Stack style={{justifyContent:"center"}} direction="row" alignItems="center" spacing={2}>
+          <Button className={Styles.btn1} variant="contained" component="label">
+            Upload
+            <input hidden accept="image/*" multiple type="file" />
+            <FileUploadIcon />
+          </Button>
+        </Stack>
+      </div>
     </div>
   );
 }
