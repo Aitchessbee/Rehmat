2cc5e9ce930faac2ac060ee01db3b057822a4b60
Aitchessbee <bediharsiddak@gmail.com>
2023-02-26 04:34:56 +0530

    updated carousel

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom"
+
 import styles from "./Styles/home.module.css"
 
 import img1 from "./Images/img1.png"
@@ -6,8 +8,11 @@ import locations from "./Images/locations.png"
 import ellipse from "./Images/ellipse.png"
 import gif1 from "./Images/gif1.gif"
 import gif2 from "./Images/gif2.gif"
-import gif3 from "./Images/gif3.gif"
 
+import carousel1 from "./Images/carousel1.png"
+import carousel2 from "./Images/carousel2.png"
+import carousel3 from "./Images/carousel3.png"
+import carousel4 from "./Images/carousel4.png"
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination } from 'swiper'
@@ -35,10 +40,33 @@ function Home() {
         slidesPerView={2.5}
         className={styles.carousel}
       >
-        <SwiperSlide><div className={styles.tile}>1</div></SwiperSlide>
-        <SwiperSlide><div className={styles.tile}>2</div></SwiperSlide>
-        <SwiperSlide><div className={styles.tile}>3</div></SwiperSlide>
-        <SwiperSlide><div className={styles.tile}>4</div></SwiperSlide>
+        <SwiperSlide>
+          <div className={styles.tile}>
+            <img src={carousel1} alt="" style={{height: "70%"}}/>
+            <div>NEED MEDICAL ATTENTION?<br /> REGISTER HERE TO GET FREE CONSULTATION</div>
+            <Link to="" className={styles.button}>SEEK HELP</Link>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+            <div className={styles.tile}>
+              <img src={carousel2} alt="" style={{height: "70%"}}/>
+              <div>1 FREE CONSULTATION A DAY CAN SAVE THE LIVES OF MILLIONS OF REFUGEES</div>
+              <Link to="" className={styles.button}>REGISTER AS A DOCTOR</Link>
+            </div>
+        </SwiperSlide>
+        <SwiperSlide><div className={styles.tile}>
+            <img src={carousel3} alt="" style={{height: "70%"}}/>
+            <div>HERE'S YOUR CHANCE TO DO YOUR BIT, AND CONTRIBUTE TO A POSITIVE CAUSE</div>
+            <Link to="" className={styles.button}>DONATE NOW!</Link>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={styles.tile}>
+            <img src={carousel4} alt="" style={{height: "70%"}}/>
+            <div>DO YOU WANT TO WORK FOR THE PLIGHT OF REFUGEES, AND HELP THEM SUSTAIN THEIR LIVES?</div>
+            <Link to="" className={styles.button}>VOLUNTEER TO HELP!</Link>
+          </div>
+        </SwiperSlide>
         ...
       </Swiper>
 
@@ -50,7 +78,11 @@ function Home() {
           <div className={styles.columns}>
             <div className={`${styles.aboutUs} ${styles.column}`}>
               <div className={styles.aboutUsHeading}>ABOUT US</div>
-              <div className={styles.aboutUsContent}>REHMAT: Refugee Health Management System  is a non-profit organization, that aims to aid refugees, and provide them with consultation free of cost. <br /> <br /> Keeping in mind the SDGs we have established a network of doctors, specialised across various domains.
+              <div className={styles.aboutUsContent}>REHMAT: Refugee Health Management System  is a non-profit organisation that aims to aid refugees, and provide them health consultation free of cost. <br />
+Refugees often don't get the best facilities, and have to suffer in queues to get treated. <br />
+Keeping in mind the SDGs we have established a network of doctors, specialised across various domains. <br />
+Our doctors and volunteers will provide online consultation free of cost. These doctors will serve as the first point of contact for refugees, and will treat mild cases directly, allowing the on field doctors to focus on more severe cases. <br /> <br />
+We aim to tie up with local governments, get donations from people, and also send volunteers and our team with resources to offline camps,
               </div>
             </div>
 
