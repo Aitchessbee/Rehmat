74c33f2e0742cf32bddfad32b324504920438869
Arvinder Singh Kandola <askandola2@gmail.com>
2023-02-26 08:34:07 +0530

    Add allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool, default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rehmat-api.ccstiet.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
